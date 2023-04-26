b3a7443a94160ca0169242b2c3fc7597565bf82e
unit-kin <akkangara8@gmail.com>
2023-04-26 08:29:34 -0700

    shell0.c betty checks

---
+++ shell0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Hello, world!\n");
+	return (0);
+}
+
+/* 
+ * This code was authored by Alvin Kang'ara and Aron Mang'ati
+ * as part of a project for a programming course. 
+ * Our goal was to create a simple "Hello, world!" program
+ * that would pass the Betty checks and demonstrate good coding practices.
+ */ 
+
