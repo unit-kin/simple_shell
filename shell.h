7721458fbe0335890a3c4ac07460e576da0db48c
R-oxy <aronmangati@gmail.com>
2023-04-27 12:56:34 +0000

    Adds files

--- shell.h
+++ shell.h
@@ -6,11 +6,23 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <errno.h>
 
 #define MAX_PATH_LENGTH 1024
+#define MAX_INPUT_SIZE 1024
 #define BUFFER_SIZE 1024
 extern char **environ;
+#define BUFSIZE 1024
+#define TOK_DELIM " \t\r\n\a"
+#define CMD_DELIM ";"
 
+void exit_shell(void);
+int execute_command(char **args, int status);
+int execute_pipeline(char ***args, int num_commands);
+char **split_line(char *line, char *delimiter);
+char **split_commands(char *line);
+char **split_arguments(char *line);
+int shell_loop(void);
 void prompt(void);
 char **parse_args(char *line);
 char *find_path(char *command);
@@ -25,5 +37,6 @@ int set_env(char **env, char *name, char *value);
 int unset_env(char **env, char *name);
 void handle_setenv(char **cmd_args);
 void handle_unsetenv(char **cmd_args);
+int handle_cd(char **cmd_args);
 
 #endif
